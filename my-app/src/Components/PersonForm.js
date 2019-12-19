4934a8a8877399e5640e436a026fa91c9b040c9b
Nhan <natenguyen51294@gmail.com>
2019-12-19 15:07:04 -0800

    added drop down for role

--- my-app/src/Components/PersonForm.js
+++ my-app/src/Components/PersonForm.js
@@ -4,9 +4,9 @@ import * as Yup from 'yup';
 import axios from 'axios';
 
 const PersonForm = ({ values, errors, touched, status }) => {
-  // console.log(values);
-  // console.log(errors);
-  // console.log(touched);
+  console.log(values);
+  console.log(errors);
+  console.log(touched);
 
   const [person, setPerson] = useState([]);
 
@@ -31,6 +31,16 @@ const PersonForm = ({ values, errors, touched, status }) => {
         {touched.email && errors.email && (                             //Validation syntax from YUP
           <p>*{errors.email}</p>
         )}
+
+        <Field as='select' id='role' name='role'>
+          <option disabled>Choose a role</option>
+          <option value='UI-Dev'>UI Dev</option>
+          <option value='UX-Dev'>UX Dev</option>
+          <option value='ReactI-Dev'>React I Dev</option>
+          <option value='ReactII-Dev'>React II Dev</option>
+          <option value='Backend-Dev'>Backend Dev</option>
+        </Field>
+
         <label> Terms and conditions
           <Field type='checkbox' name='terms' checked={values.terms} />
         </label>
@@ -43,6 +53,7 @@ const PersonForm = ({ values, errors, touched, status }) => {
           <li>Name: {e.username}</li>
           <li>Password: {e.password}</li>
           <li>Email: {e.email}</li>
+          <li>Role: {e.role}</li>
         </ul>
       })}
 
@@ -52,7 +63,7 @@ const PersonForm = ({ values, errors, touched, status }) => {
 
 // Formik syntax to wrap around components
 const FormikPersonForm = withFormik({
-  mapPropsToValues({ username, password, email, terms }) {
+  mapPropsToValues({ username, password, email, role, terms }) {
     // desconstructor props into username, password, email, terms for dry code
     // console.log(props)
     return {
@@ -60,6 +71,7 @@ const FormikPersonForm = withFormik({
       username: username || '',
       password: password || '',
       email: email || '',
+      role: role || 'Choose a role',
       terms: terms || false
     };
   },
@@ -68,7 +80,8 @@ const FormikPersonForm = withFormik({
   validationSchema: Yup.object().shape({
     username: Yup.string().required(),
     password: Yup.string().required(),
-    email: Yup.string().email().required(),
+    email: Yup.string().required(),
+    role: Yup.string().required(),
     terms: Yup.boolean().oneOf([true], `Must accept Terms and Conditions`)
   }),
 
