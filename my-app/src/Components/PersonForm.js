2ce7ec1202225e7d618101a8174d41c1eab645f8
Nhan <natenguyen51294@gmail.com>
2019-12-19 13:07:36 -0800

    added function to save data when form is submitted

--- my-app/src/Components/PersonForm.js
+++ my-app/src/Components/PersonForm.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const PersonForm = ({ values, errors, touched }) => {
+const PersonForm = ({ values, errors, touched, status }) => {
   // console.log(values);
   // console.log(errors);
   // console.log(touched);
+
+  const [person, setPerson] = useState([]);
+
+  // added useEffect in order to see the changes happening after we submit the form
+  useEffect(() => {
+    console.log(`status has been updated`, status);
+
+    // rendering the data we got after the form was submitted
+    status && setPerson(person => [...person, status]);
+  }, [status])
   return (
     <div>React Formik
       <Form>
@@ -22,41 +32,54 @@ const PersonForm = ({ values, errors, touched }) => {
         {touched.email && errors.email && (                             //Validation syntax from YUP
           <p>*{errors.email}</p>
         )}
-        <label> Terms of services
+        <label> Terms and conditions
           <Field type='checkbox' name='terms' checked={values.terms} />
         </label>
         <button type='submit'>Submit!</button>
       </Form>
+
+      {/* adding structure in order to save the data users are putting in */}
+      {person.map(e => {
+        return <ul key={e.name}>
+          <li>Name: {e.username}</li>
+          <li>Password: {e.password}</li>
+          <li>Email: {e.email}</li>
+        </ul>
+      })}
+
     </div>
   )
 }
 
 // Formik syntax to wrap around components
 const FormikPersonForm = withFormik({
-  mapPropsToValues(props) {
+  mapPropsToValues({ username, password, email, terms }) {
+    // desconstructor props into username, password, email, terms for dry code
+    // console.log(props)
     return {
-
       // Getting the intitial states for these keys values from App.js, if there is none, we can set one up to whichever we want with the 'string' quotation
-      username: props.username || '',
-      password: props.password || '',
-      email: props.email || '',
-      terms: props.terms || false
+      username: username || '',
+      password: password || '',
+      email: email || '',
+      terms: terms || false
     };
   },
 
   //Validation syntax from YUP
   validationSchema: Yup.object().shape({
-    username: Yup.string().required(`Give me your name`),
-    password: Yup.string().required(`Give me your password`),
-    email: Yup.string().required(`Give me your email`)
+    username: Yup.string().required(),
+    password: Yup.string().required(),
+    email: Yup.string().email().required(),
+    terms: Yup.boolean().oneOf([true], `Must accept Terms and Conditions`)
   }),
 
-  async handleSubmit(values, formikBag) {
+  async handleSubmit(values, { setStatus }) {
     console.log(`SUBMITTINGGGG`, values);
 
     try {
       const res = await axios.post(`https://reqres.in/api/users`, values);
-      console.log(`Success!!!`, res)
+      console.log(`Success!!!`, res);
+      setStatus(res.data);
     }
     catch (err) {
       console.log(err)
